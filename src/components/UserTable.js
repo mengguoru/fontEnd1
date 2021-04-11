592e12818096790c65622c4d0e39dfc6aea8b606
mengguoru <2506839494@qq.com>
2021-04-11 11:01:17 +0800

    post表单，新增用户

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class UserTable extends Component {
 
-    constructor() {
-        super();
+    emptyUser = {
+        username: '',
+        birthday: '',
+        sex: '',
+        address: ''
+    };
+
+    constructor(props) {
+        super(props);
         this.state = {
-            users: []
-        }
+            users: [],
+
+            showAddNewUserDialog: true,
+            newUser: this.emptyUser
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.createUser = this.createUser.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        let newUser = {...this.state.newUser};
+        newUser[name] = value
+
+        this.setState({newUser});
     }
 
     componentDidMount() {
@@ -22,24 +46,91 @@ class UserTable extends Component {
             })
     }
 
+    changeShowAddNewUserDialoge = () => {
+        this.setState({
+            showAddNewUserDialog: !this.state.showAddNewUserDialog
+        })
+    }
+
+    //提交新建用户的表单
+    createUser(event) {
+
+        event.preventDefault();
+
+
+        const {newUser} = this.state;
+
+        // gradle = "男"
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newUser)
+        };
+
+        fetch("http://localhost:8082/user/insert", requestOptions)
+            .then(response => response.json())
+            .then(response => console.log(response));
+    }
+
+
     render() {
 
-        return <div className="userTable">
-        <table>
-            {
-                this.state.users.map(user => {
+        const renderAddNewUserDialog = () => {
 
-                    return (<tr>
-                        <td>{user.id}</td>
-                        <td>{user.username} </td>
-                        <td>{user.birthday}</td>
-                        <td>{user.sex} </td>
-                        <td>{user.address}</td>
-                    </tr>);
-                })
+            if (this.state.showAddNewUserDialog) {
+                return (
+                    <div>
+
+                        <form onSubmit={this.createUser}>
+                            姓名: <input type="text" name="userName" onChange={this.handleChange}/><br/>
+                            性别：
+                            <input type="radio" value="Male" name="gender" onChange={this.handleChange}/> 男
+                            <input type="radio" value="Female" name="gender" onChange={this.handleChange}/> 女
+
+                            <br/>
+                            <input type="date" name="birthday" onChange={this.handleChange}/> <br/>
+
+
+                            地址: <input type="text" name="address" onChange={this.handleChange}/><br/>
+
+                            <button type="submit">保存</button>
+                            &nbsp;
+                            <button type="button" onClick={this.changeShowAddNewUserDialoge}>取消</button>
+                        </form>
+
+                        <br></br>
+
+
+                    </div>
+
+                );
             }
-        </table>
-    </div>;
+        }
+
+        return <div className="userTable">
+            <button onClick={this.changeShowAddNewUserDialoge}>
+                新增
+            </button>
+
+            {renderAddNewUserDialog()}
+
+            <table>
+                {
+                    this.state.users.map(user => {
+
+                        return (<tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.username} </td>
+                            <td>{user.birthday}</td>
+                            <td>{user.sex} </td>
+                            <td>{user.address}</td>
+                        </tr>);
+                    })
+                }
+            </table>
+
+        </div>;
     }
 }
 
